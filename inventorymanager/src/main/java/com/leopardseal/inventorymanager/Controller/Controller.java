50641b291f678bbd9512d666fbaf9eceadb94123
aarushth <aarush.thadani@outlook.com>
2025-05-06 21:54:08 -0700

    workign on jwt tokens

--- inventorymanager/src/main/java/com/leopardseal/inventorymanager/Controller/Controller.java
+++ inventorymanager/src/main/java/com/leopardseal/inventorymanager/Controller/Controller.java
@@ -54,70 +54,70 @@ public class Controller {
 			logger.info(msg);
 		}
 	}
-	static GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-	.setAudience(Collections.singletonList("354946788079-aermo39q0o3gshsgf46oqhkicovqcuo8.apps.googleusercontent.com"))
-    .build();
+	// static GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+	// .setAudience(Collections.singletonList("354946788079-aermo39q0o3gshsgf46oqhkicovqcuo8.apps.googleusercontent.com"))
+    // .build();
 	
-   	@Configuration
-	public class WebConfiguration implements WebMvcConfigurer {
-		@Override
-		public void addInterceptors(InterceptorRegistry registry) {
-			registry.addInterceptor(new MyHandlerInterceptor());
-		}
-	}
+   	// @Configuration
+	// public class WebConfiguration implements WebMvcConfigurer {
+	// 	@Override
+	// 	public void addInterceptors(InterceptorRegistry registry) {
+	// 		registry.addInterceptor(new MyHandlerInterceptor());
+	// 	}
+	// }
 
 
-    @Component
-    public class MyHandlerInterceptor implements HandlerInterceptor {
+    // @Component
+    // public class MyHandlerInterceptor implements HandlerInterceptor {
 
 		
-		@Autowired
-		MyHandlerInterceptor() {
-			// this.currentUser = currentUser;
-		}
-		// private CurrentUser currentUser;
+	// 	@Autowired
+	// 	MyHandlerInterceptor() {
+	// 		// this.currentUser = currentUser;
+	// 	}
+	// 	// private CurrentUser currentUser;
 
-        @Override
-        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception {
+    //     @Override
+    //     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)throws Exception {
 			
-			try {
-				String googleIdToken = request.getHeader("Authorization");
-				GoogleIdToken idToken = verifier.verify(googleIdToken);
-				if (idToken == null) {
-					throw new MyException("invalid token");
-				}
-				Payload payload = idToken.getPayload();
-				// if(!myUsersRepository.existsByEmail(payload.getEmail())){
-				// 	
-				// }
-				logger.info(payload.getEmail());
-				MyUsers user = myUsersRepository.findByEmail(payload.getEmail()).get();
-				if(user == null){
-					throw new MyException("user not in system");
-				}
-				// System.out.println(user.getEmail());
-				if(user.getImgUrl() == null){
-					user.setImgUrl((String) payload.get("picture"));
-					myUsersRepository.save(user);
-				}
-				request.setAttribute("userId", user.getId());
-      			// request.setAttribute("userId", user.getId());
-				// LoggedUserContext.setCurrentLoggedUser(user);
-			} catch (GeneralSecurityException | IOException e) {
-				throw new MyException("general security exception");
-			}catch(MyException e){
-				throw new MyException(e.getMessage());
-			}
-		return true;
-        }
-    }
+	// 		try {
+	// 			String googleIdToken = request.getHeader("Authorization");
+	// 			GoogleIdToken idToken = verifier.verify(googleIdToken);
+	// 			if (idToken == null) {
+	// 				throw new MyException("invalid token");
+	// 			}
+	// 			Payload payload = idToken.getPayload();
+	// 			// if(!myUsersRepository.existsByEmail(payload.getEmail())){
+	// 			// 	
+	// 			// }
+	// 			logger.info(payload.getEmail());
+	// 			MyUsers user = myUsersRepository.findByEmail(payload.getEmail()).get();
+	// 			if(user == null){
+	// 				throw new MyException("user not in system");
+	// 			}
+	// 			// System.out.println(user.getEmail());
+	// 			if(user.getImgUrl() == null){
+	// 				user.setImgUrl((String) payload.get("picture"));
+	// 				myUsersRepository.save(user);
+	// 			}
+	// 			request.setAttribute("userId", user.getId());
+    //   			// request.setAttribute("userId", user.getId());
+	// 			// LoggedUserContext.setCurrentLoggedUser(user);
+	// 		} catch (GeneralSecurityException | IOException e) {
+	// 			throw new MyException("general security exception");
+	// 		}catch(MyException e){
+	// 			throw new MyException(e.getMessage());
+	// 		}
+	// 	return true;
+    //     }
+    // }
 
-    @ControllerAdvice
-    public static class MyExceptionHandler {
-        @ExceptionHandler(MyException.class)
-        @ResponseBody
-        public ResponseEntity handelr() {
-            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        }
-    }
+    // @ControllerAdvice
+    // public static class MyExceptionHandler {
+    //     @ExceptionHandler(MyException.class)
+    //     @ResponseBody
+    //     public ResponseEntity handelr() {
+    //         return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+    //     }
+    // }
 }
--- inventorymanager/src/main/java/com/leopardseal/inventorymanager/Controller/LoginController.java
+++ inventorymanager/src/main/java/com/leopardseal/inventorymanager/Controller/LoginController.java
@@ -1,68 +1,95 @@
 package com.leopardseal.inventorymanager.Controller;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
 
 
+import java.util.Collections;
+import java.util.Date;
 
 import com.leopardseal.inventorymanager.Repository.*;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.leopardseal.inventorymanager.Controller.Controller.MyException;
 import com.leopardseal.inventorymanager.Entity.*;
 
 @RestController
 public class LoginController {
 
-
+    @Value("${app.jwt.secret}")
+    private String SECRET_KEY;
+    private static final long EXPIRATION_MS = 1800000;
+    
     @Autowired
     private MyUserRepository myUsersRepository;
 
     private static Logger logger = LoggerFactory.getLogger(LoginController.class);
-    // static GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-    // .setAudience(Collections.singletonList("354946788079-aermo39q0o3gshsgf46oqhkicovqcuo8.apps.googleusercontent.com"))
-    // .build();
-    // @RequestMapping("/test")
-    // public String test(){
-    //     return "Connected!!";
-    // }
 
 
+    static GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
+        .setAudience(Collections.singletonList("354946788079-aermo39q0o3gshsgf46oqhkicovqcuo8.apps.googleusercontent.com"))
+        .build();
+	
     @GetMapping("/login")
-    public ResponseEntity login(@RequestAttribute("userId") Long userId){
+    public ResponseEntity login(@RequestHeader("Authorization") String googleIdToken){
         // return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-
-        Optional<MyUsers> user = null;
-        if(myUsersRepository.existsById(userId)){
-            user = myUsersRepository.findById(userId);
+        try {
+            
+            GoogleIdToken idToken = verifier.verify(googleIdToken);
+            if (idToken == null) {
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+            Payload payload = idToken.getPayload();
+            // if(!myUsersRepository.existsByEmail(payload.getEmail())){
+            // 	
+            // }
+            logger.info(payload.getEmail());
+            MyUsers user = myUsersRepository.findByEmail(payload.getEmail()).get();
+            if(user == null){
+                return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+            }
+            // System.out.println(user.getEmail());
+            if(user.getImgUrl() == null){
+                user.setImgUrl((String) payload.get("picture"));
+                myUsersRepository.save(user);
+            }
+            String jwt = generateJwtToken(user.getEmail());
+            return ResponseEntity.ok(Collections.singletonMap("token", jwt));
+            // request.setAttribute("userId", user.getId());
+        } catch (GeneralSecurityException | IOException e) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }catch(MyException e){
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         }
-        // System.out.println();
-        return new ResponseEntity<MyUsers>(user.get(), HttpStatus.OK);
-        // // try{
-        // //     GoogleIdToken idToken = verifier.verify(googleIdToken);
-        //     if (idToken != null) {
-        //         Payload payload = idToken.getPayload();
-        //         if(!myUsersRepository.existsByEmail(payload.getEmail())){
-        //             return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        //         }
-        //         MyUsers myUser = myUsersRepository.findByEmail(payload.getEmail());
-        //         if(myUser.getPicture() == null){
-        //             myUser.setPicture((String) payload.get("picture"));
-        //             myUsersRepository.save(myUser);
-        //         }
-        //         return new ResponseEntity<MyUsers>(myUser, HttpStatus.OK);
-        //     }else{
-        //         return new ResponseEntity(HttpStatus.UNAUTHORIZED);
-        //     }
-        // }catch(GeneralSecurityException | IOException e) {
-        //     return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-        // }
+        
+        // return new ResponseEntity<MyUsers>(user.get(), HttpStatus.OK);
+        
+    }
+
+    private String generateJwtToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
     }
 }
