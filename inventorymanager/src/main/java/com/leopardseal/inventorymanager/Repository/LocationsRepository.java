00858dbf6da8ead8822fbd55c30aa38d499d6402
aarushth <aarush.thadani@outlook.com>
2025-05-28 21:43:46 -0700

    fixed search

--- inventorymanager/src/main/java/com/leopardseal/inventorymanager/Repository/LocationsRepository.java
+++ inventorymanager/src/main/java/com/leopardseal/inventorymanager/Repository/LocationsRepository.java
@@ -16,9 +16,9 @@ public interface LocationsRepository extends CrudRepository<Locations, Long> {
 
     Optional<Locations> findLocationById(Long id);
 
-    @Query("SELECT l.id, l.org_id, l.barcode, l.description, l.image_url FROM locations l WHERE l.org_id = :orgId AND ((l.name LIKE %:query%) OR (l.barcode LIKE %:query%) OR (l.description LIKE %:query%))")
+    @Query("SELECT l.id, l.name, l.org_id, l.barcode, l.description, l.image_url FROM locations l WHERE l.org_id = :orgId AND ((l.name LIKE :query) OR (l.barcode LIKE :query) OR (l.description LIKE :query))")
     List<Locations> findAllLocationsByQuery(Long orgId, String query);
 
-    @Query("SELECT l.id, l.org_id, l.barcode, l.description, l.image_url FROM locations l WHERE l.org_id = :orgId AND l.barcode = :barcode")
+    @Query("SELECT l.id, l.name, l.org_id, l.barcode, l.description, l.image_url FROM locations l WHERE l.org_id = :orgId AND l.barcode = :barcode")
     List<Locations> findAllLocationsByBarcode(Long orgId, String barcode);
 }
